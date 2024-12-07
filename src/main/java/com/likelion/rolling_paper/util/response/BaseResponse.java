bb8787efc749c1018fba78b4980bd74c564ffd04
MinseoKangQ <kms02171@naver.com>
2024-12-07 20:41:27 +0900

    :art: 기본 응답 코드

---
+++ src/main/java/com/likelion/rolling_paper/util/response/BaseResponse.java
@@ -0,0 +1,35 @@
+package com.likelion.rolling_paper.util.response;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+import org.example.gather_back_end.util.exception.BaseErrorCode;
+
+@Getter
+@ToString
+@RequiredArgsConstructor
+public class BaseResponse {
+
+    private final String timestamp = LocalDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME);
+
+    private final Boolean isSuccess;
+
+    private final String code;
+
+    private final String message;
+
+    public static BaseResponse of(Boolean isSuccess, BaseErrorCode errorCode) {
+        return new BaseResponse(isSuccess, errorCode.getCode(), errorCode.getMessage());
+    }
+
+    public static BaseResponse of(Boolean isSuccess, BaseErrorCode errorCode, String message) {
+        return new BaseResponse(isSuccess, errorCode.getCode(), message);
+    }
+
+    public static BaseResponse of(Boolean isSuccess, String code, String message) {
+        return new BaseResponse(isSuccess, code, message);
+    }
+}
