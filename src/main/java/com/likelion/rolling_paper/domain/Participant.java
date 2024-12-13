bf1b706fa7e2351637fe1027f87e1e151f3e663f
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2024-12-13 17:10:07 +0900

    🎨 [add] 참여자 엔티티 작성

---
+++ src/main/java/com/likelion/rolling_paper/domain/Participant.java
@@ -0,0 +1,22 @@
+package com.likelion.rolling_paper.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "participant")
+public class Participant {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "participant_id")
+    private Long id;
+}
