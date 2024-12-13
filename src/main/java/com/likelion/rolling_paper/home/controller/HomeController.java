3684b5b5edd2b830602e82b74fc3ab41d008700e
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2024-12-13 19:13:22 +0900

    ➕ [add] 스프링 시큐리티 의존성 추가

--- src/main/java/com/likelion/rolling_paper/home/controller/HomeController.java
+++ src/main/java/com/likelion/rolling_paper/home/controller/HomeController.java
@@ -3,6 +3,8 @@ package com.likelion.rolling_paper.home.controller;
 import com.likelion.rolling_paper.util.response.SuccessResponse;
 import lombok.RequiredArgsConstructor;
 import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,11 +13,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/home")
 public class HomeController implements HomeControllerApi{
     @Override
+    @PostMapping
     public SuccessResponse<?> createRollingPaperHome(Authentication authentication) {
         return null;
     }
 
     @Override
+    @GetMapping("/all")
     public SuccessResponse<?> getMyRollingPaperHome(Authentication authentication) {
         return null;
     }
