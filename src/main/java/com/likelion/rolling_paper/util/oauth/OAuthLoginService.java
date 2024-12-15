c1173a17a4647f1684e5b39c7c155b6db074db19
MinseoKangQ <kms02171@naver.com>
2024-12-15 14:37:53 +0900

    :sparkles: Service 로직 작성

---
+++ src/main/java/com/likelion/rolling_paper/util/oauth/OAuthLoginService.java
@@ -0,0 +1,36 @@
+package com.likelion.rolling_paper.util.oauth;
+
+import com.likelion.rolling_paper.domain.User;
+import com.likelion.rolling_paper.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class OAuthLoginService {
+    private final UserRepository userRepository;
+    private final AuthTokensGenerator authTokensGenerator;
+    private final RequestOAuthInfoService requestOAuthInfoService;
+
+    public AuthTokens login(OAuthLoginParams params) {
+        OAuthInfoResponse oAuthInfoResponse = requestOAuthInfoService.request(params);
+        Long memberId = findOrCreateMember(oAuthInfoResponse);
+        return authTokensGenerator.generate(memberId);
+    }
+
+    private Long findOrCreateMember(OAuthInfoResponse oAuthInfoResponse) {
+        return userRepository.findByEmail(oAuthInfoResponse.getEmail())
+                .map(User::getId)
+                .orElseGet(() -> newMember(oAuthInfoResponse));
+    }
+
+    private Long newMember(OAuthInfoResponse oAuthInfoResponse) {
+        User user = User.builder()
+//                .email(oAuthInfoResponse.getEmail())
+                .nickname(oAuthInfoResponse.getNickname())
+                .oAuthProvider(oAuthInfoResponse.getOAuthProvider())
+                .build();
+
+        return userRepository.save(user).getId();
+    }
+}
