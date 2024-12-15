c4f20a71263135ca4b8002cf0f4b32cff43c4cac
MinseoKangQ <kms02171@naver.com>
2024-12-15 14:36:32 +0900

    :sparkles: JwtTokenProvider 작성

--- src/main/java/com/likelion/rolling_paper/util/jwt/util/JwtTokenProvider.java
+++ src/main/java/com/likelion/rolling_paper/util/jwt/util/JwtTokenProvider.java
@@ -1,6 +1,9 @@
 package com.likelion.rolling_paper.util.jwt.util;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import java.nio.charset.StandardCharsets;
 import java.util.Date;
 import javax.crypto.SecretKey;
@@ -9,17 +12,43 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
-public class JwtUtil {
+public class JwtTokenProvider {
 
     private static SecretKey secretKey;
 
-    public JwtUtil(@Value("${spring.jwt.secret}") String secret) {
+    public JwtTokenProvider(@Value("${spring.jwt.secret}") String secret) {
         secretKey = new SecretKeySpec(
             secret.getBytes(StandardCharsets.UTF_8),
             Jwts.SIG.HS256.key().build().getAlgorithm()
         );
     }
 
+    public String generate(String subject, Date expiredAt) {
+        return Jwts.builder()
+                .setSubject(subject)
+                .setExpiration(expiredAt)
+                .signWith(secretKey, SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public String extractSubject(String accessToken) {
+        Claims claims = parseClaims(accessToken);
+        return claims.getSubject();
+    }
+
+    private Claims parseClaims(String accessToken) {
+        try {
+            return Jwts.parser()
+                    .verifyWith(secretKey)
+                    .build()
+                    .parseClaimsJws(accessToken)
+                    .getBody();
+        } catch (ExpiredJwtException e) {
+            return e.getClaims();
+        }
+    }
+
+    /**
     // 현재 사용중인 username 받기 - oauth2를 통해 제공받은 이메일
     public static String getUsername(String token) {
         return Jwts
@@ -53,12 +82,7 @@ public class JwtUtil {
     }
 
     public String createJwt(String username, String role, Long expiredMs) {
-        return Jwts.builder()
-                .claim("username", username)
-                .claim("role", role)
-                .issuedAt(new Date(System.currentTimeMillis()))
-                .expiration(new Date(System.currentTimeMillis() + expiredMs)) // token 만료시간
-                .signWith(secretKey)
-                .compact();
+
     }
+     **/
 }
