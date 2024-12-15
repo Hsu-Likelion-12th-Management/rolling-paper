e016180afdec1049866f8a1ba08c3fa9a41743d0
MinseoKangQ <kms02171@naver.com>
2024-12-15 14:29:18 +0900

    :construction: 임시 주석 처리 해제

--- src/main/java/com/likelion/rolling_paper/util/jwt/util/JwtFilter.java
+++ src/main/java/com/likelion/rolling_paper/util/jwt/util/JwtFilter.java
@@ -1,76 +1,76 @@
-//package com.likelion.rolling_paper.util.jwt.util;
-//
-//import com.likelion.rolling_paper.util.jwt.dto.CustomOAuth2User;
-//import com.likelion.rolling_paper.util.jwt.dto.UserDto;
-//import jakarta.servlet.FilterChain;
-//import jakarta.servlet.ServletException;
-//import jakarta.servlet.http.HttpServletRequest;
-//import jakarta.servlet.http.HttpServletResponse;
-//import java.io.IOException;
-//import lombok.RequiredArgsConstructor;
-//import lombok.extern.slf4j.Slf4j;
-//import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-//import org.springframework.security.core.Authentication;
-//import org.springframework.security.core.context.SecurityContextHolder;
-//import org.springframework.web.filter.OncePerRequestFilter;
-//
-//@Slf4j
-//@RequiredArgsConstructor
-//public class JwtFilter extends OncePerRequestFilter {
-//
-//    private final JwtUtil jwtUtil;
-//
-//    @Override
-//    protected void doFilterInternal(HttpServletRequest request,HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-//
-//        String authorization = request.getHeader("Authorization");
-//
-//        // Authorization 헤더 검증
-//        if (authorization == null || !authorization.startsWith("Bearer ")) {
-//
-//            log.info("[JwtFilter 클래스][doFilterInternal 메소드] token is null ");
-//            filterChain.doFilter(request, response);
-//
-//            // 조건이 해당되면 메소드 종료 (필수)
-//            return;
-//        }
-//
-//        // 토큰
-//        String token = authorization.split(" ")[1];
-//
-//        // 토큰 소멸 시간 검증
-//        if (jwtUtil.isExpired(token)) {
-//
-//            log.info("[JwtFilter 클래스][doFilterInternal 메소드] token is expired ");
-//            filterChain.doFilter(request, response);
-//
-//            // 조건이 해당되면 메소드 종료 (필수)
-//            return;
-//        }
-//
-//        // 토큰에서 username & role 획득
-//        String username = jwtUtil.getUsername(token);
-//        String role = jwtUtil.getRole(token);
-//
-//        // userDto 생성하여 값 set
-//        UserDto userDto = UserDto.builder()
-//                .username(username)
-//                .role(role)
-//                .build();
-//
-//        // UserDetails에 회원 정보 객체 담기
-//        CustomOAuth2User customOAuth2User = new CustomOAuth2User(userDto);
-//
-//        // 스프링 시큐리티 인증 토큰 생성
-//        Authentication authToken = new UsernamePasswordAuthenticationToken(
-//                customOAuth2User,
-//                null,
-//                customOAuth2User.getAuthorities()
-//        );
-//
-//        // 세션에 사용자 등록
-//        SecurityContextHolder.getContext().setAuthentication(authToken);
-//
-//        filterChain.doFilter(request, response);
-//    }
-//}
+package com.likelion.rolling_paper.util.jwt.util;
+
+import com.likelion.rolling_paper.util.jwt.dto.CustomOAuth2User;
+import com.likelion.rolling_paper.util.jwt.dto.UserDto;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@Slf4j
+@RequiredArgsConstructor
+public class JwtFilter extends OncePerRequestFilter {
+
+    private final JwtUtil jwtUtil;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        String authorization = request.getHeader("Authorization");
+
+        // Authorization 헤더 검증
+        if (authorization == null || !authorization.startsWith("Bearer ")) {
+
+            log.info("[JwtFilter 클래스][doFilterInternal 메소드] token is null ");
+            filterChain.doFilter(request, response);
+
+            // 조건이 해당되면 메소드 종료 (필수)
+            return;
+        }
+
+        // 토큰
+        String token = authorization.split(" ")[1];
+
+        // 토큰 소멸 시간 검증
+        if (jwtUtil.isExpired(token)) {
+
+            log.info("[JwtFilter 클래스][doFilterInternal 메소드] token is expired ");
+            filterChain.doFilter(request, response);
+
+            // 조건이 해당되면 메소드 종료 (필수)
+            return;
+        }
+
+        // 토큰에서 username & role 획득
+        String username = jwtUtil.getUsername(token);
+        String role = jwtUtil.getRole(token);
+
+        // userDto 생성하여 값 set
+        UserDto userDto = UserDto.builder()
+                .username(username)
+                .role(role)
+                .build();
+
+        // UserDetails에 회원 정보 객체 담기
+        CustomOAuth2User customOAuth2User = new CustomOAuth2User(userDto);
+
+        // 스프링 시큐리티 인증 토큰 생성
+        Authentication authToken = new UsernamePasswordAuthenticationToken(
+                customOAuth2User,
+                null,
+                customOAuth2User.getAuthorities()
+        );
+
+        // 세션에 사용자 등록
+        SecurityContextHolder.getContext().setAuthentication(authToken);
+
+        filterChain.doFilter(request, response);
+    }
+}
--- src/main/java/com/likelion/rolling_paper/util/jwt/util/JwtUtil.java
+++ src/main/java/com/likelion/rolling_paper/util/jwt/util/JwtUtil.java
@@ -1,64 +1,64 @@
-//package com.likelion.rolling_paper.util.jwt.util;
-//
-//import io.jsonwebtoken.Jwts;
-//import java.nio.charset.StandardCharsets;
-//import java.util.Date;
-//import javax.crypto.SecretKey;
-//import javax.crypto.spec.SecretKeySpec;
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.stereotype.Component;
-//
-//@Component
-//public class JwtUtil {
-//
-//    private static SecretKey secretKey;
-//
-//    public JwtUtil(@Value("${spring.jwt.secret}") String secret) {
-//        secretKey = new SecretKeySpec(
-//            secret.getBytes(StandardCharsets.UTF_8),
-//            Jwts.SIG.HS256.key().build().getAlgorithm()
-//        );
-//    }
-//
-//    // 현재 사용중인 username 받기 - oauth2를 통해 제공받은 이메일
-//    public static String getUsername(String token) {
-//        return Jwts
-//                .parser()
-//                .verifyWith(secretKey)
-//                .build()
-//                .parseSignedClaims(token)
-//                .getPayload()
-//                .get("username", String.class);
-//    }
-//
-//    public String getRole(String token) {
-//        return Jwts
-//                .parser()
-//                .verifyWith(secretKey)
-//                .build()
-//                .parseSignedClaims(token)
-//                .getPayload()
-//                .get("role", String.class);
-//    }
-//
-//    public Boolean isExpired(String token) {
-//        return Jwts
-//                .parser()
-//                .verifyWith(secretKey)
-//                .build()
-//                .parseSignedClaims(token)
-//                .getPayload()
-//                .getExpiration()
-//                .before(new Date());
-//    }
-//
-//    public String createJwt(String username, String role, Long expiredMs) {
-//        return Jwts.builder()
-//                .claim("username", username)
-//                .claim("role", role)
-//                .issuedAt(new Date(System.currentTimeMillis()))
-//                .expiration(new Date(System.currentTimeMillis() + expiredMs)) // token 만료시간
-//                .signWith(secretKey)
-//                .compact();
-//    }
-//}
+package com.likelion.rolling_paper.util.jwt.util;
+
+import io.jsonwebtoken.Jwts;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtUtil {
+
+    private static SecretKey secretKey;
+
+    public JwtUtil(@Value("${spring.jwt.secret}") String secret) {
+        secretKey = new SecretKeySpec(
+            secret.getBytes(StandardCharsets.UTF_8),
+            Jwts.SIG.HS256.key().build().getAlgorithm()
+        );
+    }
+
+    // 현재 사용중인 username 받기 - oauth2를 통해 제공받은 이메일
+    public static String getUsername(String token) {
+        return Jwts
+                .parser()
+                .verifyWith(secretKey)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .get("username", String.class);
+    }
+
+    public String getRole(String token) {
+        return Jwts
+                .parser()
+                .verifyWith(secretKey)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .get("role", String.class);
+    }
+
+    public Boolean isExpired(String token) {
+        return Jwts
+                .parser()
+                .verifyWith(secretKey)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .getExpiration()
+                .before(new Date());
+    }
+
+    public String createJwt(String username, String role, Long expiredMs) {
+        return Jwts.builder()
+                .claim("username", username)
+                .claim("role", role)
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + expiredMs)) // token 만료시간
+                .signWith(secretKey)
+                .compact();
+    }
+}
