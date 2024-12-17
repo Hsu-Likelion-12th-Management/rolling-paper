f9d9551a32df1e831242182642c3c842254c0fd4
MinseoKangQ <kms02171@naver.com>
2024-12-17 18:06:05 +0900

    :lock: Callback URL을 security에 추가

--- src/main/java/com/likelion/rolling_paper/util/config/SecurityConfig.java
+++ src/main/java/com/likelion/rolling_paper/util/config/SecurityConfig.java
@@ -37,7 +37,8 @@ public class SecurityConfig {
                                 "/v3/api-docs/**",
                                 "/swagger-ui/**",
                                 "/swagger-ui.html",
-                                "/api/auth/kakao"
+                                "/api/auth/kakao",
+                                "/api/auth/kakao/callback"
                         ).permitAll()
                         .anyRequest().authenticated()
                 )
