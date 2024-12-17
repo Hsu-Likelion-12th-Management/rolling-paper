44972345bfd56ecca49d1415e9de180d522eae4c
MinseoKangQ <kms02171@naver.com>
2024-12-17 15:36:42 +0900

    :lock: 카카오 로그인 경로 허용

--- src/main/java/com/likelion/rolling_paper/util/config/SecurityConfig.java
+++ src/main/java/com/likelion/rolling_paper/util/config/SecurityConfig.java
@@ -26,8 +26,7 @@ public class SecurityConfig {
                                 "/v3/api-docs/**",
                                 "/swagger-ui/**",
                                 "/swagger-ui.html",
-                                "/api/auth/kakao",
-                                "/**"
+                                "/api/auth/kakao"
                         ).permitAll()
                         .anyRequest().authenticated()
                 )
