ad787485c2c67fc9fcf225430537e181c3597ffd
Minseo Kang <kms02171@naver.com>
2024-12-18 16:45:47 +0900

    :sparkles: 프론트 카카오 로그인 배포 테스트 (#24)

--- src/main/java/com/likelion/rolling_paper/util/oauth/AuthController.java
+++ src/main/java/com/likelion/rolling_paper/util/oauth/AuthController.java
@@ -18,14 +18,15 @@ public class AuthController {
 
     @GetMapping("/kakao/callback")
     public void loginKakao(@RequestParam("code") String code, HttpServletResponse response) throws IOException {
-        log.info("[api/auth/kakao] 요청 들어옴");
+        log.info("[api/auth/kakao/callback] 요청 들어옴");
         AuthTokens res = oAuthLoginService.login(code);
 
         // 프론트엔드로 리다이렉트하면서 JWT 토큰 전달
-        String frontendUrl = "http://localhost:5173/home?token=" + res.getAccessToken();
+//        String frontendUrl = "http://localhost:5173/home?token=" + res.getAccessToken();
 
         // TODO: 프론트 배포 주소
-//        String frontendUrl = "https://boogiball.netlify.app/home?token=" + res.getAccessToken();
+        String frontendUrl = "https://boogiball.netlify.app/home?token=" + res.getAccessToken();
+        log.info("[api/auth/kakao/callback] https://boogiball.netlify.app/home?token= 으로 리다이렉션 보냄");
         response.sendRedirect(frontendUrl);
     }
 }
