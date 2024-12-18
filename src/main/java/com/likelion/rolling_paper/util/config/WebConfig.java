883820da7c96c7de86619e9b2788b2e691c28b8f
MinseoKangQ <kms02171@naver.com>
2024-12-18 15:32:26 +0900

    :sparkles: 프론트 배포 시 redirect 주소 임시 작성

--- src/main/java/com/likelion/rolling_paper/util/config/WebConfig.java
+++ src/main/java/com/likelion/rolling_paper/util/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "http://localhost:5173", "https://boogiball.netlify.app")
+                .allowedOrigins("http://localhost:3000", "http://localhost:5173", "http://localhost:8080", "https://boogiball.netlify.app")
                 .allowedMethods("GET", "POST", "PATCH", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true)
