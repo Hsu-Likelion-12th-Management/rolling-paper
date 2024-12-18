689c48ace7a7403d81b6862b5ae1c037a47256e6
MinseoKangQ <kms02171@naver.com>
2024-12-18 17:59:22 +0900

    :fire: 필요없는 코드 임시 주석 처리

--- src/main/java/com/likelion/rolling_paper/domain/RollingPaperHome.java
+++ src/main/java/com/likelion/rolling_paper/domain/RollingPaperHome.java
@@ -1,49 +1,49 @@
-package com.likelion.rolling_paper.domain;
-
-import com.likelion.rolling_paper.home.dto.CreateHomeReqDto;
-import com.likelion.rolling_paper.util.entity.BaseEntity;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
-@Entity
-@Getter
-@Builder
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor(access = AccessLevel.PROTECTED)
-@Table(name = "rolling_paper_home")
-public class RollingPaperHome extends BaseEntity {
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "home_id")
-    private Long id;
-
-    private String name;
-
-    @Column(name = "participants_count")
-    private Integer participantsCount;
-
-    // == 연관 관계 매핑 == //
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "owner_id")
-    private User owner;
-
-    // == 편의 메소드 ==
-    public static RollingPaperHome toEntity(User user, CreateHomeReqDto dto) {
-        return RollingPaperHome.builder()
-                .name(dto.name())
-                .participantsCount(dto.participantCount())
-                .owner(user)
-                .build();
-    }
-}
+//package com.likelion.rolling_paper.domain;
+//
+//import com.likelion.rolling_paper.home.dto.CreateHomeReqDto;
+//import com.likelion.rolling_paper.util.entity.BaseEntity;
+//import jakarta.persistence.Column;
+//import jakarta.persistence.Entity;
+//import jakarta.persistence.FetchType;
+//import jakarta.persistence.GeneratedValue;
+//import jakarta.persistence.GenerationType;
+//import jakarta.persistence.Id;
+//import jakarta.persistence.JoinColumn;
+//import jakarta.persistence.ManyToOne;
+//import jakarta.persistence.Table;
+//import lombok.AccessLevel;
+//import lombok.AllArgsConstructor;
+//import lombok.Builder;
+//import lombok.Getter;
+//import lombok.NoArgsConstructor;
+//
+//@Entity
+//@Getter
+//@Builder
+//@NoArgsConstructor(access = AccessLevel.PROTECTED)
+//@AllArgsConstructor(access = AccessLevel.PROTECTED)
+//@Table(name = "rolling_paper_home")
+//public class RollingPaperHome extends BaseEntity {
+//    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    @Column(name = "home_id")
+//    private Long id;
+//
+//    private String name;
+//
+//    @Column(name = "participants_count")
+//    private Integer participantsCount;
+//
+//    // == 연관 관계 매핑 == //
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "owner_id")
+//    private User owner;
+//
+//    // == 편의 메소드 ==
+//    public static RollingPaperHome toEntity(User user, CreateHomeReqDto dto) {
+//        return RollingPaperHome.builder()
+//                .name(dto.name())
+//                .participantsCount(dto.participantCount())
+//                .owner(user)
+//                .build();
+//    }
+//}
--- src/main/java/com/likelion/rolling_paper/home/exception/RollingPaperHomeNotFoundException.java
+++ src/main/java/com/likelion/rolling_paper/home/exception/RollingPaperHomeNotFoundException.java
@@ -1,10 +1,10 @@
-package com.likelion.rolling_paper.home.exception;
-
-
-import com.likelion.rolling_paper.util.exception.BaseException;
-
-public class RollingPaperHomeNotFoundException extends BaseException {
-    public RollingPaperHomeNotFoundException() {
-        super(RollingPaperHomeErrorCode.HOME_NOT_FOUND_EXCEPTION);
-    }
-}
+//package com.likelion.rolling_paper.home.exception;
+//
+//
+//import com.likelion.rolling_paper.util.exception.BaseException;
+//
+//public class RollingPaperHomeNotFoundException extends BaseException {
+//    public RollingPaperHomeNotFoundException() {
+//        super(RollingPaperHomeErrorCode.HOME_NOT_FOUND_EXCEPTION);
+//    }
+//}
